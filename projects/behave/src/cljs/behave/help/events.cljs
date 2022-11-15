58a8e176e2004a509c8403d3828a022b6355f5e0
Kenneth Cheung <kcheung331@gmail.com>
2022-11-14 19:15:35 -0500

    pr fixes

--- projects/behave/src/cljs/behave/help/events.cljs
+++ projects/behave/src/cljs/behave/help/events.cljs
@@ -3,16 +3,15 @@
 
 (rf/reg-event-fx
  :help/highlight-section
- (fn [{:keys [db]} [_ help-key]]
-   {:db db
-    :fx [[:dispatch [:state/set :help-current-highlighted-key help-key]]
+ (fn [_ [_ help-key]]
+   {:fx [[:dispatch [:state/set :help-current-highlighted-key help-key]]
          [:help/scroll-into-view help-key]]}))
 
 (rf/reg-fx
  :help/scroll-into-view
  (fn [help-key]
    (-> (.getElementById js/document help-key)
-       (.scrollIntoView true)))) ;should set the alignToTop=true but not working atm.
+       (.scrollIntoView true))))
 
 (rf/reg-event-db
  :help/select-tab
