9f0a023c30688f9edf938b9f6088deb0b08e22b1
Kenneth Cheung <kcheung331@gmail.com>
2023-03-09 12:58:05 -0500

    update solver test

--- projects/behave/test/cljs/behave/worksheet_events_test.cljs
+++ projects/behave/test/cljs/behave/worksheet_events_test.cljs
@@ -394,7 +394,7 @@
     ;; 1 row exist
     ;; all inputs and outputs headers and values are recorded
     ;; Use CSV to populate inputs and outputs and test against csv results -> GET FROM CONTAIN_TESTING
-    (is (some? @(rf/subscribe [:worksheet/result-table-cell-data fx/test-ws-uuid])))
+    (is (seq @(rf/subscribe [:worksheet/result-table-cell-data fx/test-ws-uuid])))
 
     (is (= @(rf/subscribe [:worksheet/result-table-cell-data fx/test-ws-uuid])
            [[0 "fbbf73f6-3a0e-4fdd-b913-dcc50d2db311" "1"]
