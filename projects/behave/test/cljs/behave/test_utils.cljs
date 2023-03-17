a28bbf584f1f68f951a7b732f83ef2d51339ed8e
Kenneth Cheung <kcheung331@gmail.com>
2023-03-17 10:19:58 -0400

    update to use reactive entity, trace changes to all views

--- projects/behave/test/cljs/behave/test_utils.cljs
+++ projects/behave/test/cljs/behave/test_utils.cljs
@@ -1,23 +1,36 @@
 (ns behave.test-utils
-  (:require [datascript.impl.entity]))
+  (:require [datascript.impl.entity]
+            [datascript.core :as d]
+            [behave.store :as s]
+            [austinbirch.reactive-entity :as re]))
 
 (defn is-entity? [e]
   (= (type e) datascript.impl.entity/Entity))
 
 (defn entity->cljs [entity]
-  (if (is-entity? entity)
-    (reduce
-     (fn [acc key]
-       (let [val (get entity key)]
-         (cond
-           (or (set? val) (seq? val))
-           (assoc acc key (mapv entity->cljs val))
+  (cond
 
-           (is-entity? val)
-           (assoc acc key (entity->cljs val))
+    (map? entity)
+    (entity->cljs (d/entity @@s/conn (:db/id entity)))
 
-           :else
-           (assoc acc key val))))
-     {}
-     (keys entity))
+    (is-entity? entity)
+    (reduce (fn [acc key]
+              (let [val (get entity key)]
+                (cond
+                  (or (set? val) (seq? val))
+                  (assoc acc key (mapv entity->cljs val))
+
+                  (is-entity? val)
+                  (assoc acc key (entity->cljs val))
+
+                  :else
+                  (assoc acc key val))))
+            {}
+            (keys entity))
+
+    :else
     entity))
+
+
+(defn re-entity->cljs [entity]
+  (entity->cljs (re/current-state entity)))
