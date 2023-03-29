a7e7c2dec84196bf6e69dc0ba9fe06b9523b0996
RJ Sheperd <rjsheperd@gmail.com>
2023-03-29 16:42:03 -0700

    Update deps.edn to work with `clj -M:poly` command
    Also adds the `:dev` alias when running via CIDER/Emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
-((nil . ((cider-default-cljs-repl . figwheel-main)
+((nil . ((cider-clojure-cli-command . "clojure")
+	 (cider-clojure-cli-aliases . ":dev")
+	 (cider-default-cljs-repl . figwheel-main)
          (cider-figwheel-main-default-options . "dev"))))
