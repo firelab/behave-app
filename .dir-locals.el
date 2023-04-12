50625e7796dbedf5b0cd7bfcd867c3ab3609a7d5
RJ Sheperd <rjsheperd@gmail.com>
2023-04-12 11:42:08 -0700

    Correct the .dir-locals.el file

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,3 @@
-((nil . ((cider-clojure-cli-command . "clojure")
-         (cider-clojure-cli-aliases . ":dev")
+((nil . ((cider-clojure-cli-aliases . "-A:dev")
          (cider-default-cljs-repl . figwheel-main)
          (cider-figwheel-main-default-options . "dev"))))
