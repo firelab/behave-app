756f24f3a5c7bb7db4c72182bb67a8d553ed4288
RJ Sheperd <rjsheperd@gmail.com>
2023-04-12 10:34:56 -0700

    Add `test` target to Makefile, make all targets .PHONY

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((nil . ((cider-clojure-cli-command . "clojure")
-	 (cider-clojure-cli-aliases . ":dev")
-	 (cider-default-cljs-repl . figwheel-main)
+         (cider-clojure-cli-aliases . ":dev")
+         (cider-default-cljs-repl . figwheel-main)
          (cider-figwheel-main-default-options . "dev"))))
