40b202b5db4c0c2b33ef8c1591cbe2b72bbbe975
RJ Sheperd <rjsheperd@gmail.com>
2023-04-22 21:09:36 -0700

    Update contain tests for multiple rows

--- projects/behave/test/cljs/behave/contain_test.cljs
+++ projects/behave/test/cljs/behave/contain_test.cljs
@@ -9,18 +9,15 @@
 
 ;; Helpers
 
-(defn within? [precision a b]
+(defn within? [a b precision]
   (> precision (- a b)))
 
 (def within-millionth? (partial within? 1e-06))
 
 ;; Tests
 
-(deftest contain-testing-simple
-  (let [row    (->> (inline-resource "public/csv/contain.csv")
-                    (parse-csv)
-                    (first))
-        module (contain/init)]
+(defn- test-contain [row-idx row]
+  (let [module (contain/init)]
 
     ;; Arrange
     (doto module
@@ -40,26 +37,20 @@
     (contain/doContainRun module)
 
     ;; Assert
-    (is (within-millionth? (get row "fireLineLength")           (contain/getFinalFireLineLength module (get-unit "ch"))))
-    (is (within-millionth? (get row "perimeterAtInitialAttack") (contain/getPerimeterAtInitialAttack module (get-unit "ch"))))
-    (is (within-millionth? (get row "perimeterAtContainment")   (contain/getPerimeterAtContainment module (get-unit "ch"))))
-    (let [expected (get row "fireSizeAtInitialAttack")
-          observed (contain/getFireSizeAtInitialAttack module (get-unit "ac"))]
-      (is (within-millionth? expected observed)
-          (str "Fire Size at Initial Attack"
-               "\n-- Expected: "
-               expected
-               "\n-- Observed: "
-               observed)))
-    (is (within-millionth? (get row "fireSize")                 (contain/getFinalFireSize module (get-unit "ac"))))
-    (is (within-millionth? (get row "containmentArea")          (contain/getFinalContainmentArea module (get-unit "ac"))))
+    (testing (str "csv row idx:" row-idx)
+      (is (within? (get row "fireLineLength")           (contain/getFinalFireLineLength module (get-unit "ch")) 1e-6))
+      (is (within? (get row "perimeterAtInitialAttack") (contain/getPerimeterAtInitialAttack module (get-unit "ch")) 1e-6))
+      (is (within? (get row "perimeterAtContainment")   (contain/getPerimeterAtContainment module (get-unit "ch")) 1e-6))
+      (is (within? (get row "fireSizeAtInitialAttack")  (contain/getFireSizeAtInitialAttack module (get-unit "ac")) 1e-6))
+      (is (within? (get row "fireSize")                 (contain/getFinalFireSize module (get-unit "ac")) 1e-6))
+      (is (within? (get row "containmentArea")          (contain/getFinalContainmentArea module (get-unit "ac")) 1e-6))
+      (is (within? (get row "timeSinceReport")          (contain/getFinalTimeSinceReport module (get-unit "min")) 1e-6))
+      (is (= (enums/contain-status (get row "containmentStatus")) (contain/getContainmentStatus module))))))
 
-    (let [expected (get row "timeSinceReport")
-          observed (contain/getFinalTimeSinceReport module (get-unit "m"))]
-      (is (within-millionth? (get row "timeSinceReport")          (contain/getFinalTimeSinceReport module (get-unit "m")))
-          (str "Time Since Report"
-               "\n-- Expected: "
-               expected
-               "\n-- Observed: "
-               observed)))
-    (is (= (enums/contain-status (get row "containmentStatus")) (contain/getContainmentStatus module)))))
+(deftest contain-testing-simple
+  (let [rows (->> (inline-resource "public/csv/contain.csv")
+                  (parse-csv))]
+    (doall
+     (map-indexed (fn [idx row-data]
+                    (test-contain idx row-data))
+                  rows))))
