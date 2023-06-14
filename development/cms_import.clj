81ec0055035941f43993d4fdff5a89ab650b3764
Kenneth Cheung <kcheung331@gmail.com>
2023-06-14 16:59:05 -0400

    add cms imports, SIGSurface, SIGMoistureScenarios, SIGBehaveRun

--- development/cms_import.clj
+++ development/cms_import.clj
@@ -13,6 +13,20 @@
   (with-open [out-file (clojure.java.io/writer f)]
     (clojure.pprint/pprint m out-file)))
 
+(defn cms-import [{:keys [behave-file
+                          sig-adapter-file
+                          out-file-name
+                          from-key
+                          to-key]}]
+  (let [behave-edn (read-string (slurp (fs/expand-home behave-file)))
+        sig-edn    (read-string (slurp (fs/expand-home sig-adapter-file)))
+        merged-edn (-> behave-edn
+                       (dissoc-in [:global from-key from-key])
+                       (update-in [:global] rename-keys {from-key to-key})
+                       (update-in [:global to-key] merge (get-in sig-edn [:global to-key])))]
+
+    (write-pprint-edn merged-edn (str "cms-exports/" out-file-name))))
+
 (comment
 
   (def surface-edn (read-string (slurp (fs/expand-home "~/Code/sig/hatchet/exports/surface.edn"))))
@@ -30,5 +44,29 @@
 
   (spit "cms-exports/SIGSurface.edn" (str sig-surface))
   (write-pprint-edn sig-surface "cms-exports/SIGSurface.edn")
+
+  (cms-import {:behave-file      "~/work/code/hatchet/behave/surface.edn"
+               :sig-adapter-file "~/work/code/hatchet/sig-adapters/SIGSurface.edn"
+               :out-file-name    "SIGSurface.edn"
+               :from-key         :Surface
+               :to-key           :SIGSurface})
+
+  (cms-import {:behave-file      "~/work/code/hatchet/behave/moistureScenarios.edn"
+               :sig-adapter-file "~/work/code/hatchet/sig-adapters/SIGMoistureScenarios.edn"
+               :out-file-name    "SIGMoistureScenarios.edn"
+               :from-key         :MoistureScenarios
+               :to-key           :SIGMoistureScenarios})
+
+  (cms-import {:behave-file      "~/work/code/hatchet/behave/crown.edn"
+               :sig-adapter-file "~/work/code/hatchet/sig-adapters/SIGCrown.edn"
+               :out-file-name    "SIGCrown.edn"
+               :from-key         :Crown
+               :to-key           :SIGCrown})
+
+  (cms-import {:behave-file      "~/work/code/hatchet/behave/behaveRun.edn"
+               :sig-adapter-file "~/work/code/hatchet/sig-adapters/SIGBehaveRun.edn"
+               :out-file-name    "SIGBehaveRun.edn"
+               :from-key         :BehaveRun
+               :to-key           :SIGBehaveRun})
 
   )
