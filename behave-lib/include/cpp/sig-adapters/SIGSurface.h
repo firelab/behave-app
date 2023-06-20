b549452d5fd40e2c683515e53b92202aa8fb512e
Kenneth Cheung <kcheung331@gmail.com>
2023-06-20 13:39:10 -0400

    expose getWindAdjustmentFactor

--- behave-lib/include/cpp/sig-adapters/SIGSurface.h
+++ behave-lib/include/cpp/sig-adapters/SIGSurface.h
@@ -62,4 +62,7 @@ public:
   double getChaparralLoadLiveMedium(LoadingUnits::LoadingUnitsEnum loadingUnits) const;
   double getChaparralLoadLiveLarge(LoadingUnits::LoadingUnitsEnum loadingUnits) const;
   double getChaparralLoadLiveVeryLarge(LoadingUnits::LoadingUnitsEnum loadingUnits) const;
+
+  // SurfaceFire Getter Methods
+  double getWindAdjustmentFactor() const;
 };
