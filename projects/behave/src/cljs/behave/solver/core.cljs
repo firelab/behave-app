523b5110577603a5487ebc02ca62e69d72815d48
Kenneth Cheung <kcheung331@gmail.com>
2023-08-04 09:52:27 -0400

    remove variables from store fire shape event

--- projects/behave/src/cljs/behave/solver/core.cljs
+++ projects/behave/src/cljs/behave/solver/core.cljs
@@ -210,13 +210,7 @@
                 (surface/getWindSpeed module
                                       (enums/speed-units "ChainsPerHour")
                                       (surface/getWindHeightInputMode module))
-                (surface/getElapsedTime module (enums/time-units "Hours"))
-                [["Surface Fire Area" (surface/getFireArea module (enums/area-units "Acres")) "ac"]
-                 ["Surface Fire Perimeter" (surface/getFirePerimeter module (enums/length-units "Chains")) "ch"]
-                 ["Surface Fire Length" (surface/getFireLength module (enums/length-units "Chains")) "ch"]
-                 ["Surface Fire Maximum Width" (surface/getMaxFireWidth module (enums/length-units "Chains")) "ch"]
-                 ["Elapsed Time" (surface/getElapsedTime module (enums/time-units "Hours")) "h"]
-                 ["Surface Fire Dir of Max Spread" (surface/getDirectionOfMaxSpread module) "deg"]]]))
+                (surface/getElapsedTime module (enums/time-units "Hours"))]))
 
 (defn run-module [{:keys [inputs all-outputs outputs row-id] :as row}
                   {:keys [init-fn
