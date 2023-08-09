6b9a6cd5c95c2f05a94815529919da4ed74fcef7
Kenneth Cheung <kcheung331@gmail.com>
2023-08-09 14:50:15 -0400

    fix contain diagram legend name

--- projects/behave/src/cljs/behave/worksheet/events.cljs
+++ projects/behave/src/cljs/behave/worksheet/events.cljs
@@ -457,14 +457,14 @@
                   :worksheet.diagram/row-id              row-id
                   :worksheet.diagram/ellipses            [(let [l (- fire-head-at-report fire-back-at-report)
                                                                 w (/ l length-to-width-ratio)]
-                                                            {:ellipse/legend-id       "firePerimiterAtReport"
+                                                            {:ellipse/legend-id       "FirePerimiterAtReport"
                                                              :ellipse/semi-major-axis (/ l 2)
                                                              :ellipse/semi-minor-axis (/ w 2)
                                                              :ellipse/rotation        90
                                                              :ellipse/color           "blue"})
                                                           (let [l (- fire-head-at-attack fire-back-at-attack)
                                                                 w (/ l length-to-width-ratio)]
-                                                            {:ellipse/legend-id       "firePerimiterAtAttack"
+                                                            {:ellipse/legend-id       "FirePerimiterAtAttack"
                                                              :ellipse/semi-major-axis (/ l 2)
                                                              :ellipse/semi-minor-axis (/ w 2)
                                                              :ellipse/rotation        90
