54a458dfa2e9a32cd710d867a3d17bc1edd3068f
Kenneth Cheung <kcheung331@gmail.com>
2023-08-22 18:43:12 -0400

    clean :tool/select-tool

--- projects/behave/src/cljs/behave/tool/events.cljs
+++ projects/behave/src/cljs/behave/tool/events.cljs
@@ -30,11 +30,13 @@
 
 (rf/reg-event-fx
  :tool/select-tool
- (fn [_ [_ tool-uuid]]
-   {:fx [[:dispatch [:state/set [:tool :selected-subtool] nil]]
-         [:dispatch [:state/set [:tool :selected-tool] tool-uuid]]
-         [:dispatch [:tool/close-tool-selector]]
-         [:dispatch [:help/select-tab {:tab :tools}]]]}))
+ (rf/path db-here)
+ (fn [{:keys [db]} [_ tool-uuid]]
+   {:fx [[:dispatch [:tool/close-tool-selector]]
+         [:dispatch [:help/select-tab {:tab :tools}]]]
+    :db (-> db
+            (dissoc :selected-subtool)
+            (assoc :selected-tool tool-uuid))}))
 
 (rf/reg-event-db
  :tool/select-subtool
@@ -43,17 +45,17 @@
    (assoc db :selected-subtool subtool-uuid)))
 
 ;;TODO update compute to actually run the selected subtool's compute fn
-(rf/reg-event-fx
+(rf/reg-event-db
  :tool/compute
- (fn [_ [_ selected-tool selected-subtool]]
-   (let [output-variables @(rf/subscribe [:subtool/output-variables selected-subtool])
-         effects          (mapv (fn [variable]
-                                  [:dispatch [:state/set [:tool
-                                                          :data
-                                                          selected-tool
-                                                          selected-subtool
-                                                          :outputs
-                                                          (:bp/uuid variable)]
-                                              (str "computed subtool:" selected-subtool)]])
-                                output-variables)]
-     {:fx effects})))
+ (rf/path db-here)
+ (fn [db [_ selected-tool selected-subtool]]
+   (let [output-variables @(rf/subscribe [:subtool/output-variables selected-subtool])]
+     (reduce (fn [db variable]
+               (assoc-in db [:data
+                             selected-tool
+                             selected-subtool
+                             :outputs
+                             (:bp/uuid variable)]
+                         (str "computed subtool:" selected-subtool)))
+             db
+             output-variables))))
