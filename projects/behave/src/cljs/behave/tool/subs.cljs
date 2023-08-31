d8cb3dff4aba8e3ae6c2c4f794c443fb6a068242
Kenneth Cheung <kcheung331@gmail.com>
2023-08-31 17:33:04 -0400

    Update Schema and views for auto compute feature

--- projects/behave/src/cljs/behave/tool/subs.cljs
+++ projects/behave/src/cljs/behave/tool/subs.cljs
@@ -36,36 +36,41 @@
  (fn [_ [_ tool-uuid]]
    (d/entity @@s/vms-conn [:bp/uuid tool-uuid])))
 
-(defn- flatten-variable-data [variable]
-  (let [variable-data (rename-keys (first (:variable/_subtool-variables variable))
+(defn- enrich-subtool-variable [subtool-variable]
+  (let [variable-data (rename-keys (first (:variable/_subtool-variables subtool-variable))
                                    {:bp/uuid :variable/uuid})]
-    (-> variable
+    (-> subtool-variable
         (dissoc :variable/_subtool-variables)
         (merge variable-data)
         (dissoc :variable/subtool-variables)
         (update :variable/kind keyword))))
 
 (reg-sub
- :subtool/input-variables
+ :subtool/encriched-subtool-variables
  (fn [_ [_ subtool-uuid]]
-   (let [subtool (d/pull @@s/vms-conn '[* {:subtool/input-variables
+   (let [subtool (d/pull @@s/vms-conn '[* {:subtool/variables
                                            [* {:variable/_subtool-variables
                                                [* {:variable/list [* {:list/options [*]}]}]}]}]
                          [:bp/uuid subtool-uuid])]
-     (->> (:subtool/input-variables subtool)
-          (mapv flatten-variable-data)
+     (->> (:subtool/variables subtool)
+          (mapv enrich-subtool-variable)
           (sort-by :subtool-variable/order)))))
 
 (reg-sub
+ :subtool/input-variables
+ (fn [[_ subtool-eid]]
+   (rf/subscribe [:subtool/encriched-subtool-variables subtool-eid]))
+ (fn [variables _]
+   (filter #(= (:subtool-variable/io %) :input) variables)))
+
+(reg-sub
  :subtool/output-variables
- (fn [_ [_ subtool-uuid]]
-   (let [subtool (d/pull @@s/vms-conn '[* {:subtool/output-variables
-                                           [* {:variable/_subtool-variables
-                                               [* {:variable/list [* {:list/options [*]}]}]}]}]
-                         [:bp/uuid subtool-uuid])]
-     (->> (:subtool/output-variables subtool)
-          (mapv flatten-variable-data)
-          (sort-by :subtool-variable/order)))))
+ (fn [[_ subtool-eid]]
+   (rf/subscribe [:subtool/encriched-subtool-variables subtool-eid]))
+ (fn [variables _]
+   (filter #(= (:subtool-variable/io %) :output) variables)))
+
+
 
 (reg-sub
  :tool/input-value
@@ -105,9 +110,10 @@
    (d/q '[:find [?output-uuids ...]
           :in $ ?uuid
           :where
-          [?s :bp/uuid ?uuid]
-          [?s :subtool/output-variables ?o]
-          [?o :bp/uuid ?output-uuids]]
+          [?s  :bp/uuid ?uuid]
+          [?s  :subtool/variables ?sv]
+          [?sv :subtool-variable/io :output]
+          [?sv :bp/uuid ?output-uuids]]
         @@s/vms-conn subtool-uuid)))
 
 (comment
