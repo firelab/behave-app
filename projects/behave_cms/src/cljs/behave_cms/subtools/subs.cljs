37968cf80b26776b8c7c49e07c0fd433910d8d8c
Kenneth Cheung <kcheung331@gmail.com>
2023-09-05 11:44:41 -0400

    sort variables

--- projects/behave_cms/src/cljs/behave_cms/subtools/subs.cljs
+++ projects/behave_cms/src/cljs/behave_cms/subtools/subs.cljs
@@ -3,26 +3,33 @@
 
 (reg-sub
  :subtool/variables
+
  (fn [[_ subtool-eid]]
    (subscribe [:pull-children :subtool/variables subtool-eid '[* {:variable/_subtool-variables [*]}]]))
+
  (fn [variables]
-   (map (fn [sv] (let [variable (get-in sv [:variable/_subtool-variables 0])]
-                   (-> sv
-                       (dissoc :variable/_subtool-variables)
-                       (merge (dissoc variable :db/id)))))
-        variables)))
+   (-> (map (fn [sv] (let [variable (get-in sv [:variable/_subtool-variables 0])]
+                       (-> sv
+                           (dissoc :variable/_subtool-variables)
+                           (merge (dissoc variable :db/id)))))
+            variables)
+       (sort-by :subtool-variable/order))))
 
 (reg-sub
  :subtool/input-variables
+
  (fn [[_ subtool-eid]]
    (subscribe [:subtool/variables subtool-eid]))
+
  (fn [variables _]
    (filter #(= (:subtool-variable/io %) :input) variables)))
 
 (reg-sub
  :subtool/output-variables
+
  (fn [[_ subtool-eid]]
    (subscribe [:subtool/variables subtool-eid]))
+
  (fn [variables _]
    (filter #(= (:subtool-variable/io %) :output) variables)))
 
@@ -30,6 +37,7 @@
 
 (reg-sub
  :tools
+
  (fn [[_ application-id]]
    (subscribe [:pull-children :application/tools application-id]))
  identity)
