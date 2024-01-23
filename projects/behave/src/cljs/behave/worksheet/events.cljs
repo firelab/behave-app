2f901c00934b4dd6286b658b6fa3e1120bf73fb0
Kenneth Cheung <kcheung331@gmail.com>
2024-01-23 13:46:26 -0500

    set default x-axis-limit after solving worksheet

--- projects/behave/src/cljs/behave/worksheet/events.cljs
+++ projects/behave/src/cljs/behave/worksheet/events.cljs
@@ -321,27 +321,29 @@
  [(rf/inject-cofx ::inject/sub (fn [[_ ws-uuid]] [:worksheet/multi-value-input-uuids ws-uuid]))
   (rf/inject-cofx ::inject/sub (fn [[_ ws-uuid]] [:worksheet ws-uuid]))]
  (fn [{worksheet               :worksheet
-       multi-value-input-uuids :worksheet/multi-value-input-uuids} _]
+       multi-value-input-uuids :worksheet/multi-value-input-uuids} [_ ws-uuid]]
    (when (pos? (count multi-value-input-uuids))
     (let [graph-setting-id (get-in worksheet [:worksheet/graph-settings :db/id])]
-      {:transact [; First clear any existing graph settings.
-                  [:db/retract graph-setting-id :graph-settings/x-axis-group-variable-uuid]
-                  [:db/retract graph-setting-id :graph-settings/y-axis-group-variable-uuid]
-                  [:db/retract graph-setting-id :graph-settings/z-axis-group-variable-uuid]
-                  ;; Then default any multi valued variables starting from the lowest to highest dimensions x->z.
-                  (cond-> {:db/id                   graph-setting-id}
+      (cond-> {:transact [; First clear any existing graph settings.
+                          [:db/retract graph-setting-id :graph-settings/x-axis-group-variable-uuid]
+                          [:db/retract graph-setting-id :graph-settings/y-axis-group-variable-uuid]
+                          [:db/retract graph-setting-id :graph-settings/z-axis-group-variable-uuid]
+                          ;; Then default any multi valued variables starting from the lowest to highest dimensions x->z.
+                          (cond-> {:db/id                   graph-setting-id}
 
-                    ;; sets default x-axis selection if available
-                    (first multi-value-input-uuids)
-                    (assoc :graph-settings/x-axis-group-variable-uuid (first multi-value-input-uuids))
+                            ;; sets default x-axis selection if available
+                            (first multi-value-input-uuids)
+                            (assoc :graph-settings/x-axis-group-variable-uuid (first multi-value-input-uuids))
 
-                    ;; sets default z-axis selection if available
-                    (second multi-value-input-uuids)
-                    (assoc :graph-settings/z-axis-group-variable-uuid (second multi-value-input-uuids))
+                            ;; sets default z-axis selection if available
+                            (second multi-value-input-uuids)
+                            (assoc :graph-settings/z-axis-group-variable-uuid (second multi-value-input-uuids))
 
-                    ;; sets default z2-axis selection if available
-                    (nth multi-value-input-uuids 2 nil)
-                    (assoc :graph-settings/z2-axis-group-variable-uuid (nth multi-value-input-uuids 2)))]}))))
+                            ;; sets default z2-axis selection if available
+                            (nth multi-value-input-uuids 2 nil)
+                            (assoc :graph-settings/z2-axis-group-variable-uuid (nth multi-value-input-uuids 2)))]}
+        (first multi-value-input-uuids)
+        (assoc :fx [[:dispatch [:worksheet/upsert-x-axis-limit ws-uuid (first multi-value-input-uuids)]]]))))))
 
 (rp/reg-event-fx
  :worksheet/toggle-graph-settings
