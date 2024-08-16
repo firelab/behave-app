9c5b3e33d92ce9731b59f5d055660d49cc722252
Kenneth Cheung <kcheung331@gmail.com>
2024-08-16 11:10:35 -0400

    remove prn statement

--- projects/behave/src/cljs/behave/wizard/subs.cljs
+++ projects/behave/src/cljs/behave/wizard/subs.cljs
@@ -375,7 +375,6 @@
              values              :conditional/values}]
            (let [{:keys [group-uuid io]} @(subscribe [:wizard/conditional-io+group-uuid
                                                       group-variable-uuid])
-                 _ (prn [group-uuid io])
                  conditional-values-set  (set values)
                  worksheet-value         (cond
                                            (= ttype :module)
