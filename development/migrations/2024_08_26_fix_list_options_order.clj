5cdcfd9ac6ca252b72bd07aaa124931e9a79d511
Kenneth Cheung <kcheung331@gmail.com>
2024-08-26 15:15:47 -0400

    fix surface fuel model order

---
+++ development/migrations/2024_08_26_fix_list_options_order.clj
@@ -0,0 +1,68 @@
+(ns migrations.2024-08-26-fix-list-options-order
+  (:require [schema-migrate.interface :as sm]
+            [datomic.api :as d]
+            [behave-cms.store :refer [default-conn]]
+            [behave-cms.server :as cms]
+            [clojure.string :as str]))
+
+;; ===========================================================================================================
+;; Initialize
+;; ===========================================================================================================
+
+(cms/init-db!)
+
+#_{:clj-kondo/ignore [:missing-docstring]}
+(def conn (default-conn))
+
+(def list-options (d/q '[:find  ?order ?name ?lo
+                         :in $
+                         :where
+                         [?l :list/name "SurfaceFuelModels"]
+                         [?l :list/options ?lo]
+                         [?lo :list-option/name ?name]
+                         [?lo :list-option/order ?order]]
+                       (d/db conn)))
+
+;; handles cases when both values left and right of slashes are number (i.e. 1/1 , 2/2)
+(def first-group-sorted
+  (->> list-options
+       (filter (fn [[_ n]]
+                 (let [[v1 v2] (map read-string (str/split (re-find #".+(?= - )" n) #"/"))]
+                   (and (number? v1) (number? v2)))))
+       (sort-by (fn [[_ n]]
+                  (let [[v1 v2] (map read-string (str/split (re-find #".+(?= - )" n) #"/"))]
+                    [v1 v2])))))
+
+;; handles all other cases (i.e. GS1/121)
+(def second-group-sorted
+  (->> list-options
+       (remove (fn [[_ n]]
+                 (let [[v1 v2] (map read-string (str/split (re-find #".+(?= - )" n) #"/"))]
+                   (and (number? v1) (number? v2)))))
+       (sort-by (fn [[_ n]]
+                  (let [[v1 v2] (map read-string (str/split (re-find #".+(?= - )" n) #"/"))]
+                    [v1 v2])))))
+
+(def all-list-options-sorted (concat first-group-sorted second-group-sorted))
+
+(def payload
+  (map-indexed
+   (fn [idx [_ _ eid]]
+     {:db/id             eid
+      :list-option/order idx})
+   all-list-options-sorted))
+
+;; ===========================================================================================================
+;; Transact Payload
+;; ===========================================================================================================
+
+(comment
+  #_{:clj-kondo/ignore [:missing-docstring]}
+  (def tx-data (d/transact conn payload)))
+
+;; ===========================================================================================================
+;; In case we need to rollback.
+;; ===========================================================================================================
+
+(comment
+  (sm/rollback-tx! conn @tx-data))
