b76ce2a51b3b988de2ae3a4aa00c4f2683ca1fe8
Kenneth Cheung <kcheung331@gmail.com>
2024-09-17 18:55:13 -0400

    update migration script

--- development/migrations/2024_09_16_add_ignite_module_to_surface.clj
+++ development/migrations/2024_09_16_add_ignite_module_to_surface.clj
@@ -2,8 +2,7 @@
   (:require [schema-migrate.interface :as sm]
             [datomic.api :as d]
             [behave-cms.store :refer [default-conn]]
-            [behave-cms.server :as cms]
-            [cms-import :refer [add-export-file-to-conn]]))
+            [behave-cms.server :as cms]))
 
 ;; ===========================================================================================================
 ;; Overview
@@ -18,11 +17,11 @@
 #_{:clj-kondo/ignore [:missing-docstring]}
 (def conn (default-conn))
 
-
 ;; ===========================================================================================================
 ;; payload
 ;; ===========================================================================================================
 
+#_{:clj-kondo/ignore [:missing-docstring]}
 (def payload
   [{:db/id                 (sm/t-key->eid conn "behaveplus:surface:output:fire_behavior:surface_fire")
     :group/order           18
@@ -74,8 +73,7 @@
                                     :group/group-variables [(let [cpp-uuids (sm/cpp-uuids conn {:cpp-namespace "global"
                                                                                                 :cpp-class     "SIGIgnite"
                                                                                                 :cpp-function  "setAirTemperature"})]
-                                                              {:db/id                                 -3
-                                                               :variable/_group-variables             (sm/name->eid conn :variable/name "Air Temperature")
+                                                              {:variable/_group-variables             (sm/name->eid conn :variable/name "Air Temperature")
                                                                :group-variable/order                  0
                                                                :group-variable/cpp-namespace          (:cpp-namespace cpp-uuids)
                                                                :group-variable/cpp-class              (:cpp-class cpp-uuids)
@@ -85,42 +83,53 @@
                                                                :group-variable/help-key               "behaveplus:surface:input:weather:air_temperature:air_temperature:help"})]}
 
                                    {:group/name "Fuel Shading from the Sun"
-
                                     :group/order           0
                                     :group/translation-key "behaveplus:surface:input:weather:fuel_shading_from_the_sun"
                                     :group/group-variables [(let [cpp-uuids (sm/cpp-uuids conn {:cpp-namespace "global"
                                                                                                 :cpp-class     "SIGIgnite"
                                                                                                 :cpp-function  "setSunShade"}) ]
-                                                              {:db/id                                 -4
-                                                               :variable/_group-variables             (sm/name->eid conn :variable/name "Fuel Shading from the Sun")
+                                                              {:variable/_group-variables             (sm/name->eid conn :variable/name "Fuel Shading from the Sun")
                                                                :group-variable/order                  0
                                                                :group-variable/cpp-namespace          (:cpp-namespace cpp-uuids)
                                                                :group-variable/cpp-class              (:cpp-class cpp-uuids)
                                                                :group-variable/cpp-function           (:cpp-function cpp-uuids)
                                                                :group-variable/translation-key        "behaveplus:surface:input:weather:fuel_shading_from_the_sun:fuel_shading_from_the_sun"
                                                                :group-variable/result-translation-key "behaveplus:surface:result:weather:fuel_shading_from_the_sun:fuel_shading_from_the_sun"
-                                                               :group-variable/help-key               "behaveplus:surface:input:weather:fuel_shading_from_the_sun:fuel_shading_from_the_sun:help"})]}]}
+                                                               :group-variable/help-key               "behaveplus:surface:input:weather:fuel_shading_from_the_sun:fuel_shading_from_the_sun:help"})]}]}])}
 
-      {:submodule/name            "Ignite"
-       :submodule/io              :output
-       :submodule/order           5
-       :submodule/translation-key "behaveplus:surface:input:ignite"
-       :submodule/groups          [{:group/name            "Ignition Probability"
-                                    :group/translation-key "behaveplus:surface:output:ignite:ignition_probability"
-                                    :group/group-variables [(let [cpp-uuids (sm/cpp-uuids conn {:cpp-namespace "global"
-                                                                                                :cpp-class     "SIGIgnite"
-                                                                                                :cpp-function  "getFirebrandIgnitionProbability"})]
-                                                              {:db/id                                 -5
-                                                               :variable/_group-variables             (sm/name->eid conn :variable/name "Probability of Ignition")
-                                                               :group-variable/order                  0
-                                                               :group-variable/cpp-namespace          (:cpp-namespace cpp-uuids)
-                                                               :group-variable/cpp-class              (:cpp-class cpp-uuids)
-                                                               :group-variable/cpp-function           (:cpp-function cpp-uuids)
-                                                               :group-variable/translation-key        "behaveplus:surface:output:ignite:ignition_probability:ignition_probability"
-                                                               :group-variable/result-translation-key "behaveplus:surface:result:weather:ignition_probability:ignition_probability"
-                                                               :group-variable/help-key               "behaveplus:surface:output:ignite:ignition_probability:ignition_probability:help"})]}]}])}
+   {:db/id            (sm/t-key->eid conn "behaveplus:surface:output:fire_behavior")
+    :submodule/groups (sm/postwalk-insert
+                       [{:group/name            "Ignition"
+                         :group/order           3
+                         :group/translation-key "behaveplus:surface:output:fire_behavior:ignition"
+                         :group/group-variables [(let [cpp-uuids (sm/cpp-uuids conn {:cpp-namespace "global"
+                                                                                     :cpp-class     "SIGIgnite"
+                                                                                     :cpp-function  "getFirebrandIgnitionProbability"})]
+                                                   {:group-variable/order                  0
+                                                    :variable/_group-variables             (sm/name->eid conn :variable/name "Probability of Ignition")
+                                                    :group-variable/cpp-namespace          (:cpp-namespace cpp-uuids)
+                                                    :group-variable/cpp-class              (:cpp-class cpp-uuids)
+                                                    :group-variable/cpp-function           (:cpp-function cpp-uuids)
+                                                    :group-variable/translation-key        "behaveplus:surface:output:fire_behavior:probability_of_ignition:probability_of_ignition"
+                                                    :group-variable/result-translation-key "behaveplus:surface:result:fire_behavior:probability_of_ignition:probability_of_ignition"
+                                                    :group-variable/help-key               "behaveplus:surface:output:fire_behavior:probability_of_ignition:probability_of_ignition:help"})]}])}
 
    (sm/->link -1 -2)])
+
+#_{:clj-kondo/ignore [:missing-docstring]}
+(def translation-payload
+  (sm/build-translations-payload
+   conn
+   100
+   {"behaveplus:surface:output:fire_behavior:surface_fire:1_h_fuel_moisture"                  "1-h Fuel Moisture"
+    "behaveplus:surface:input:weather:1_h_fuel_moisture:1_h_fuel_moisture"                    "1-h Fuel Moisture"
+    "behaveplus:surface:input:weather"                                                        "Weather"
+    "behaveplus:surface:input:weather:air_temperature"                                        "Air Temperature"
+    "behaveplus:surface:input:weather:air_temperature:air_temperature"                        "Air Temperature"
+    "behaveplus:surface:input:weather:fuel_shading_from_the_sun"                              "Fuel Shading From the Sun"
+    "behaveplus:surface:input:weather:fuel_shading_from_the_sun:fuel_shading_from_the_sun"    "Fuel Shading From the Sun"
+    "behaveplus:surface:output:fire_behavior:ignition"                                        "Ignition"
+    "behaveplus:surface:output:fire_behavior:probability_of_ignition:probability_of_ignition" "Probability of Ignition"}))
 
 ;; ===========================================================================================================
 ;; Transact Payload
@@ -129,20 +138,20 @@
 (comment
   (do
     #_{:clj-kondo/ignore [:missing-docstring]}
-    (def tx-data (d/transact conn payload))
+    (def tx-data (d/transact conn (concat payload translation-payload)))
 
     #_{:clj-kondo/ignore [:missing-docstring]}
     (def add-conditional-payload
       [{:db/id                  (sm/t-key->eid conn "behaveplus:surface:input:weather")
         :submodule/conditionals (sm/postwalk-insert
-                                 [{:conditional/group-variable-uuid (sm/t-key->uuid conn "behaveplus:surface:output:ignite:ignition_probability:ignition_probability")
+                                 [{:conditional/group-variable-uuid (sm/t-key->uuid conn "behaveplus:surface:output:fire_behavior:probability_of_ignition:probability_of_ignition")
                                    :conditional/type                :group-variable
                                    :conditional/operator            :equal
                                    :conditional/values              ["true"]}])}
 
        {:db/id                  (sm/t-key->eid conn "behaveplus:surface:input:fuel_moisture")
         :submodule/conditionals (sm/postwalk-insert
-                                 [{:conditional/group-variable-uuid (sm/t-key->uuid conn "behaveplus:surface:output:ignite:ignition_probability:ignition_probability")
+                                 [{:conditional/group-variable-uuid (sm/t-key->uuid conn "behaveplus:surface:output:fire_behavior:probability_of_ignition:probability_of_ignition")
                                    :conditional/type                :group-variable
                                    :conditional/operator            :equal
                                    :conditional/values              ["true"]}])}])
