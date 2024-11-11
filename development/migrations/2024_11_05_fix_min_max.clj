a776402361af7487e634889c480452f2704caca9
RJ Sheperd <rjsheperd@gmail.com>
2024-11-10 17:53:05 -0800

    Fix Variable Min/Max values

---
+++ development/migrations/2024_11_05_fix_min_max.clj
@@ -0,0 +1,53 @@
+(ns migrations.2024-11-05-fix-min-max
+  (:require [schema-migrate.interface :as sm]
+            [datomic.api :as d]
+            [behave-cms.store :refer [default-conn]]
+            [behave-cms.server :as cms]
+            [csv-parser.interface :refer [fetch-csv]]
+            [map-utils.interface :refer [index-by]]))
+
+;; ===========================================================================================================
+;; Initialize
+;; ===========================================================================================================
+
+(cms/init-db!)
+
+#_{:clj-kondo/ignore [:missing-docstring]}
+(def conn (default-conn))
+
+(d/q '[:find ?e
+       :where [?e :db/ident :variable/bp6-code]]
+     (d/db conn))
+
+#_{:clj-kondo/ignore [:missing-docstring]}
+(def bp6-code->eids
+  (index-by :key 
+            (d/q '[:find ?e ?v
+                   :keys :eid :key
+                   :where [?e :variable/bp6-code ?v]]
+                 (d/db conn))))
+
+#_{:clj-kondo/ignore [:missing-docstring]}
+(def vars-min-max (fetch-csv "cms-exports/vars_min_max.csv"))
+
+#_{:clj-kondo/ignore [:missing-docstring :shadowed-var]}
+(def payload (map (fn [{:keys [key min max]}]
+               (-> {:db/id (get-in bp6-code->eids [key :eid])}        
+                   (assoc :variable/maximum (parse-double max))
+                   (assoc :variable/minimum (parse-double min))))
+             vars-min-max))
+
+;; ===========================================================================================================
+;; Transact Payload
+;; ===========================================================================================================
+
+(comment
+  #_{:clj-kondo/ignore [:missing-docstring]}
+  (def tx-data (d/transact conn payload)))
+
+;; ===========================================================================================================
+;; In case we need to rollback.
+;; ===========================================================================================================
+
+(comment
+  (sm/rollback-tx! conn @tx-data))
