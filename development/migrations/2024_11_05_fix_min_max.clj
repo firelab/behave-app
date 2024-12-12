84be2014382bbbeb45addf3dab1d705baf46b1b2
RJ Sheperd <rjsheperd@gmail.com>
2024-12-12 13:00:28 -0800

    Removes existing maximum, minimum, default-value values

--- development/migrations/2024_11_05_fix_min_max.clj
+++ development/migrations/2024_11_05_fix_min_max.clj
@@ -1,5 +1,6 @@
 (ns migrations.2024-11-05-fix-min-max
   (:require [schema-migrate.interface :as sm]
+            [clojure.string :as str]
             [datomic.api :as d]
             [behave-cms.store :refer [default-conn]]
             [behave-cms.server :as cms]
@@ -10,32 +11,43 @@
 ;; Initialize
 ;; ===========================================================================================================
 
-(cms/init-db!)
-
-#_{:clj-kondo/ignore [:missing-docstring]}
-(def conn (default-conn))
-
-(d/q '[:find ?e
-       :where [?e :db/ident :variable/bp6-code]]
-     (d/db conn))
-
-#_{:clj-kondo/ignore [:missing-docstring]}
-(def bp6-code->eids
-  (index-by :key 
-            (d/q '[:find ?e ?v
-                   :keys :eid :key
-                   :where [?e :variable/bp6-code ?v]]
-                 (d/db conn))))
-
-#_{:clj-kondo/ignore [:missing-docstring]}
-(def vars-min-max (fetch-csv "cms-exports/vars_min_max.csv"))
 
 #_{:clj-kondo/ignore [:missing-docstring :shadowed-var]}
-(def payload (map (fn [{:keys [key min max]}]
-               (-> {:db/id (get-in bp6-code->eids [key :eid])}        
-                   (assoc :variable/maximum (-> max (str/replace #"," "") parse-double))
-                   (assoc :variable/minimum (-> max (str/replace #"," "") parse-double))))
-             vars-min-max))
+(do
+
+  (cms/init-db!)
+
+  (def conn (default-conn))
+
+  ;;; Remove existing min/max/default_value
+  (def vars
+    (d/q '[:find [?e ...]
+           :where [?e :variable/maximum ?n]]
+         (d/db conn)))
+
+  (def retract-existing-payload
+    (apply concat
+           (map (fn [eid]
+                  [[:db/retract eid :variable/default-value]
+                   [:db/retract eid :variable/maximum]
+                   [:db/retract eid :variable/minimum]]) vars)))
+
+  ;;; Add new variable min/max
+  (def bp6-code->eids
+    (index-by :key
+              (d/q '[:find ?e ?v
+                     :keys :eid :key
+                     :where [?e :variable/bp6-code ?v]]
+                   (d/db conn))))
+
+  (def vars-min-max (fetch-csv "cms-exports/vars_min_max.csv"))
+
+  (def new-min-max-payload
+    (map (fn [{:keys [key min max]}]
+           (-> {:db/id (get-in bp6-code->eids [key :eid])}
+               (assoc :variable/maximum (-> max (str/replace #"," "") parse-double))
+               (assoc :variable/minimum (-> min (str/replace #"," "") parse-double))))
+         vars-min-max)))
 
 ;; ===========================================================================================================
 ;; Transact Payload
@@ -43,11 +55,15 @@
 
 (comment
   #_{:clj-kondo/ignore [:missing-docstring]}
-  (def tx-data (d/transact conn payload)))
+  (do
+    (def tx-1 (d/transact conn retract-existing-payload))
+    (def tx-2 (d/transact conn new-min-max-payload))))
 
 ;; ===========================================================================================================
 ;; In case we need to rollback.
 ;; ===========================================================================================================
 
 (comment
-  (sm/rollback-tx! conn @tx-data))
+  (do
+    (sm/rollback-tx! conn @tx-1)
+    (sm/rollback-tx! conn @tx-2)))
