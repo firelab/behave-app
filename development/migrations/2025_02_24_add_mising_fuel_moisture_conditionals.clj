7c0b708da40229e720dd7c912d405ba13d7179ce
Kenneth Cheung <kcheung331@gmail.com>
2025-02-27 14:13:12 -0500

    add missing conditional for 110

--- development/migrations/2025_02_24_add_mising_fuel_moisture_conditionals.clj
+++ development/migrations/2025_02_24_add_mising_fuel_moisture_conditionals.clj
@@ -21,20 +21,23 @@
 ;; Payload
 ;; ===========================================================================================================
 
+(def ten-h
+  #{"110"} )
+
 #_{:clj-kondo/ignore [:missing-docstring]}
 (def live-herbacheous
   #{"111" "110" "155"})
-
-#_{:clj-kondo/ignore [:missing-docstring]}
-(def all-codes-to-unhide
-  (set live-herbacheous))
 
 #_{:clj-kondo/ignore [:missing-docstring]}
 (def list-options
   (:list/options (d/entity (d/db conn) (sm/name->eid conn :list/name "SurfaceFuelModels"))))
 
 #_{:clj-kondo/ignore [:missing-docstring]}
+(def individual-size-class-ten-h-fuel-moisture-conditional (d/entity (d/db conn) 4611681620380881711))
+
+#_{:clj-kondo/ignore [:missing-docstring]}
 (def individual-size-class-live-herbacheous-fuel-moisture-conditional (d/entity (d/db conn) 4611681620380881713))
+
 
 #_{:clj-kondo/ignore [:missing-docstring]}
 (defn add-values-to-conditional [conditional-entity values-to-add]
@@ -44,7 +47,8 @@
 
 #_{:clj-kondo/ignore [:missing-docstring]}
 (def payload
-  [(add-values-to-conditional individual-size-class-live-herbacheous-fuel-moisture-conditional live-herbacheous)])
+  [(add-values-to-conditional individual-size-class-ten-h-fuel-moisture-conditional ten-h)
+   (add-values-to-conditional individual-size-class-live-herbacheous-fuel-moisture-conditional live-herbacheous)])
 
 ;; ===========================================================================================================
 ;; Transact Payload
