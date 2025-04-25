57410da3f0288df709cbe357bacb05f715cfcb41
RJ Sheperd <rjsheperd@gmail.com>
2025-04-25 12:12:22 -0700

    Update interface, deps.edn

--- components/jcef/src/jcef/interface.clj
+++ components/jcef/src/jcef/interface.clj
@@ -1,1 +1,18 @@
-(ns jcef.interface)
+(ns jcef.interface
+  (:require [jcef.core :as c]))
+
+(def ^{:arglists '([options])
+       :doc      "Creates a CEF app frame with the following options map:
+             - `:title`           [Req.] - Title of the app.
+             - `:url`             [Req.] - URL to start the browser at.
+             - `:on-close`        [Opt.] - Function to execute when the window closes.
+             - `:use-osr?`        [Opt.] - Use Windowless Rendering (Default: false)
+             - `:is-transparent?` [Opt.] - Transparent window (Default: false)
+             - `:address-bar?`    [Opt.] - Show an address bar. (Default: false)
+
+             Returns a map with:
+             - `:app`     - `JFrame` Application
+             - `:browser` - `CefBrowser`
+             - `:client`  - `CefClient`"}
+
+  create-cef-app! c/create-cef-app!)
