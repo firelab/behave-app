b4a0bee28841ea6fa6d7ea3e029a6841ebc8a409
Kenneth Cheung <kcheung331@gmail.com>
2025-10-03 14:30:38 -0400

    fix format

--- bases/behave_components/src/cljs/behave/stories/matrix_table_stories.cljs
+++ bases/behave_components/src/cljs/behave/stories/matrix_table_stories.cljs
@@ -14,22 +14,20 @@
                                  :column-headers [{:name "Column 1" :key :column1}
                                                   {:name "Column 2" :key :column2}
                                                   {:name "Column 3" :key :column3}]
+                                 :row-headers    [{:name "Row 1" :key :row1}
+                                                  {:name "Row 2" :key :row2}
+                                                  {:name "Row 3" :key :row3}]
+                                 :data           {[:row1 :column1] 1
+                                                  [:row1 :column2] 2
+                                                  [:row1 :column3] 3
 
-                                 :row-headers [{:name "Row 1" :key :row1}
-                                               {:name "Row 2" :key :row2}
-                                               {:name "Row 3" :key :row3}]
+                                                  [:row2 :column1] 4
+                                                  [:row2 :column2] 5
+                                                  [:row2 :column3] 6
 
-                                 :data {[:row1 :column1] 1
-                                        [:row1 :column2] 2
-                                        [:row1 :column3] 3
-
-                                        [:row2 :column1] 4
-                                        [:row2 :column2] 5
-                                        [:row2 :column3] 6
-
-                                        [:row3 :column1] 7
-                                        [:row3 :column2] 8
-                                        [:row3 :column3] 9}}
+                                                  [:row3 :column1] 7
+                                                  [:row3 :column2] 8
+                                                  [:row3 :column3] 9}}
                                 args)}))
 
 (def ^:export Default (template))
@@ -38,11 +36,9 @@
                                 :column-headers [{:name "0" :key 0}
                                                  {:name "1" :key 1}
                                                  {:name "2" :key 2}]
-
                                 :row-headers    [{:name "0" :key 0}
                                                  {:name "1" :key 1}
                                                  {:name "2" :key 2}]
-
                                 :data           {[0 0] 1
                                                  [0 1] 2
                                                  [0 2] 3
