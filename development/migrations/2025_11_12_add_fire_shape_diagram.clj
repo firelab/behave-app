9838a7f2c16e66dd1043f4ad59f92f78c89a8661
Kenneth Cheung <kcheung331@gmail.com>
2025-11-12 16:52:16 -0500

    move migration script

--- development/migrations/2025_11_12_add_fire_shape_diagram.clj
+++ development/migrations/2025_11_12_add_fire_shape_diagram.clj
@@ -1,4 +1,4 @@
-(ns migrations.template
+(ns migrations.2025-11-12-add-fire-shape-diagram
   (:require [schema-migrate.interface :as sm]
             [datomic.api :as d]
             [behave-cms.store :refer [default-conn]]
@@ -17,12 +17,39 @@
 #_{:clj-kondo/ignore [:missing-docstring]}
 (def conn (default-conn))
 
+(def fire-shape-diagram-eid
+  (d/q '[:find ?e .
+         :in $
+         :where
+         [?e :diagram/type :fire-shape]]
+       (d/db conn)))
+
+(def new-fire-shape-diagram-output-group-variable
+  (sm/->group-variable
+   conn
+   {:db/id            -1
+    :parent-group-eid (sm/t-key->eid conn "behaveplus:surface:output:size:surface___fire_size")
+    :order            7
+    :variable-eid     (sm/name->eid conn :variable/name "Fire Shape Diagram")
+    :hide-results?    true
+    :translation-key  "behaveplus:surface:output:size:surface___fire_size:fire-shape-diagram"}))
+
 ;; ===========================================================================================================
 ;; Payload
 ;; ===========================================================================================================
 
 #_{:clj-kondo/ignore [:missing-docstring]}
-(def payload [])
+(def payload
+  (concat [new-fire-shape-diagram-output-group-variable
+           {:db/id                  fire-shape-diagram-eid
+            :diagram/group-variable -1}
+           {:db/id                  (sm/t-key->eid conn "behaveplus:surface:input:size")
+            :submodule/conditionals [(sm/->conditional conn {:ttype               :group-variable
+                                                             :operator            :equal
+                                                             :values              #{"true"}
+                                                             :group-variable-uuid (:bp/uuid new-fire-shape-diagram-output-group-variable)})]}]
+          (sm/build-translations-payload conn 2 {"behaveplus:surface:output:size:surface___fire_size:fire-shape-diagram" "Fire Shape Diagram"})))
+
 
 ;; ===========================================================================================================
 ;; Transact Payload
