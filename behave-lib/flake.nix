48fffb5067ad5f7dcce4b769a822e7ff375edc16
RJ Sheperd <rjsheperd@gmail.com>
2026-01-22 16:47:37 -0800

    Add Nix Flake builder

--- behave-lib/flake.nix
+++ behave-lib/flake.nix
@@ -2,9 +2,11 @@
   description = "Emscripten build environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
+
+  inputs.self.submodules = true;
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -15,31 +17,67 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             gnumake
-            cmake
+            cmakeMinimal
             llvm
             clang
             emscripten
             fd  # for finding webidl_binder.py
+            python315
           ];
 
           shellHook = ''
             export EM_CACHE=$PWD/.em_cache
-            export WEB_IDL=$(fd -1 webidl_binder.py /nix/store)
-            export WEBIDL="${WEB_IDL%.*}"
+            export WEBIDL="python ${pkgs.emscripten}/share/emscripten/tools/webidl_binder.py"
             echo "Environment ready!"
             echo "EM_CACHE: $EM_CACHE"
-            echo "WEB_IDL: $WEB_IDL"
+            echo "WEBIDL: $WEBIDL"
           '';
         };
 
         # Convenience app to run the build
-        apps.build = {
+        apps.default = {
           type = "app";
           program = toString (pkgs.writeShellScript "build.sh" ''
             export EM_CACHE=$PWD/.em_cache
-            export WEB_IDL=$(${pkgs.fd}/bin/fd -1 webidl_binder.py /nix/store)
+            export WEBIDL="python ${pkgs.emscripten}/share/emscripten/tools/webidl_binder.py"
             ${pkgs.gnumake}/bin/make install
           '');
+        };
+
+        # Convenience package for `nix build`
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "behave-lib";
+          version = "0.1.0";
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            gnumake
+            cmakeMinimal
+            llvm
+            clang
+            emscripten
+            fd
+            python315
+          ];
+
+          configurePhase = ''
+            export EM_CACHE=$TMPDIR/.em_cache
+            export WEBIDL="python ${pkgs.emscripten}/share/emscripten/tools/webidl_binder.py"
+            mkdir -p build
+            cd build
+            ${pkgs.cmakeMinimal}/bin/cmake ..
+          '';
+
+          buildPhase = ''
+            export EM_CACHE=$TMPDIR/.em_cache
+            export WEBIDL="python ${pkgs.emscripten}/share/emscripten/tools/webidl_binder.py"
+            ${pkgs.gnumake}/bin/make
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r * $out/
+          '';
         };
       }
     );
