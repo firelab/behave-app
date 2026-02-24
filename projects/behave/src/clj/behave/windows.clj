27fa2d48e762b5e457ccd631c8df59b986a4c551
RJ Sheperd <rjsheperd@gmail.com>
2026-02-23 20:49:43 -0800

    Missing files

---
+++ projects/behave/src/clj/behave/windows.clj
@@ -0,0 +1,30 @@
+(ns behave.windows
+  (:import [org.cef CefApp]))
+
+(defonce ^:private *windows (atom {}))
+
+(defn register-window!
+  "Registers a window in the registry."
+  [window-id window-state]
+  (swap! *windows assoc window-id window-state))
+
+(defn deregister-window!
+  "Deregisters a window. If no windows remain, disposes CefApp and exits."
+  [window-id]
+  (let [remaining (swap! *windows dissoc window-id)]
+    (when (empty? remaining)
+      (.dispose (CefApp/getInstance))
+      (.exit (Runtime/getRuntime) 0))))
+
+(defn window-count
+  "Returns the number of open windows."
+  []
+  (count @*windows))
+
+
+(comment
+
+  (map #(get-in % [:app :browser]) (vals @*windows))
+
+
+  )
