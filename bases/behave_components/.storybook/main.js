ac4da515e22b2393bdbf7c9c7a4e94f23610680b
RJ Sheperd <rjsheperd@gmail.com>
2026-04-08 08:28:23 -0700

    Fix storybook

--- bases/behave_components/.storybook/main.js
+++ bases/behave_components/.storybook/main.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports = {
   "stories": [{
     directory: "../resources/public/js/stories/",
@@ -13,5 +15,39 @@ module.exports = {
   "framework": {
     "name": "@storybook/react-webpack5",
     "options": {}
+  },
+  // shadow-cljs compiles stories to CJS using Object.defineProperty(module.exports, "default", ...)
+  // Storybook 8's static CSF indexer expects ES module `export default` syntax, so we
+  // provide a custom indexer that parses the compiled ClojureScript output directly.
+  "indexers": async (existingIndexers) => {
+    const cljsIndexer = {
+      test: /behave\.stories\./,
+      createIndex: async (fileName, { makeTitle }) => {
+        const content = fs.readFileSync(fileName, 'utf8');
+
+        const titleMatch = content.match(/"title":\s*"([^"]+)"/);
+        const title = titleMatch ? titleMatch[1] : makeTitle(fileName);
+
+        const exportPattern = /Object\.defineProperty\(module\.exports,\s*"([^"]+)"/g;
+        const exportNames = [];
+        let match;
+        while ((match = exportPattern.exec(content)) !== null) {
+          const name = match[1];
+          if (name !== 'default' && name !== '__esModule') {
+            exportNames.push(name);
+          }
+        }
+
+        return exportNames.map(exportName => ({
+          type: 'story',
+          importPath: fileName,
+          exportName,
+          name: exportName,
+          title,
+          tags: [],
+        }));
+      }
+    };
+    return [cljsIndexer, ...existingIndexers];
   }
 }
