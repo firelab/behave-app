809cd5c5ec4820294b0004ccd60eace4b5bc8f5f
Kenneth Cheung <kcheung331@gmail.com>
2026-06-04 17:48:30 -0400

    refactor out radio group components

--- projects/behave/src/cljs/behave/components/table_settings/views.cljs
+++ projects/behave/src/cljs/behave/components/table_settings/views.cljs
@@ -4,59 +4,76 @@
             [behave.components.table-settings.subs]
             [re-frame.core          :refer [dispatch subscribe]]))
 
-(defn table-settings-modal [ws-uuid]
+(defn- multi-mvi-radio-group
+  [{:keys [label selected on-select group-vars]}]
+  [c/radio-group
+   {:label   label
+    :options (mapv (fn [{:keys [gv-uuid var-name]}]
+                     {:value     var-name
+                      :label     var-name
+                      :on-change #(on-select gv-uuid)
+                      :checked?  (= selected gv-uuid)})
+                   group-vars)}])
+
+(defn- single-mvi-radio-group
+  [{:keys [label selected mvi-uuid mvi-name on-select]}]
+  [c/radio-group
+   {:label   label
+    :options [{:value     mvi-name
+               :label     mvi-name
+               :on-change #(on-select mvi-uuid)
+               :checked?  (= selected mvi-uuid)}
+              {:value     "Outputs"
+               :label     "Outputs"
+               :on-change #(on-select "outputs")
+               :checked?  (= selected "outputs")}]}])
+
+(defn table-settings-modal
+  "A modal component for displaying table settings"
+  [ws-uuid]
   (let [multi-valued-input-uuids @(subscribe [:worksheet/multi-value-input-uuids ws-uuid])
         multi-valued-input-count (count multi-valued-input-uuids)
-        group-variables          (map #(deref (subscribe [:wizard/group-variable %])) multi-valued-input-uuids)]
-    (letfn [(radio-group [{:keys [label attr fallback-attr]}]
-              (let [selected? (or (first @(subscribe [:table-settings/attr-values ws-uuid attr]))
-                                  (first @(subscribe [:worksheet/get-graph-settings-attr ws-uuid fallback-attr])))]
-                [c/radio-group
-                 {:label   label
-                  :options (mapv (fn [{group-var-uuid :bp/uuid}]
-                                   (let [var-name @(subscribe [:wizard/gv-uuid->resolve-result-variable-name group-var-uuid])]
-                                     {:value     var-name
-                                      :label     var-name
-                                      :on-change #(dispatch [:table-settings/update-attr
-                                                             ws-uuid
-                                                             attr
-                                                             group-var-uuid])
-                                      :checked?  (= selected? group-var-uuid)}))
-                                 group-variables)}]))
-            (single-mvi-radio-group [{:keys [label this-attr mvi-gv-uuid]}]
-              (let [this-attr-val (first @(subscribe [:table-settings/attr-values ws-uuid this-attr]))
-                    mvi-name      @(subscribe [:wizard/gv-uuid->resolve-result-variable-name mvi-gv-uuid])]
-                [c/radio-group
-                 {:label   label
-                  :options [{:value     mvi-name
-                             :label     mvi-name
-                             :on-change #(dispatch [:table-settings/update-attr ws-uuid this-attr mvi-gv-uuid])
-                             :checked?  (= this-attr-val mvi-gv-uuid)}
-                            {:value     "Outputs"
-                             :label     "Outputs"
-                             :on-change #(dispatch [:table-settings/update-attr ws-uuid this-attr "outputs"])
-                             :checked?  (= this-attr-val "outputs")}]}]))]
-      [c/modal {:title          "Table Settings"
-                :close-on-click #(dispatch [:table-settings/toggle])
-                :content        [:<>
-                                 (when (= multi-valued-input-count 1)
-                                   (let [mvi-gv-uuid (:bp/uuid (first group-variables))]
-                                     [:<>
-                                      [single-mvi-radio-group {:label       "Row variable:"
-                                                               :this-attr   :table-settings/row-group-variable-uuid
-                                                               :mvi-gv-uuid mvi-gv-uuid}]
-                                      [single-mvi-radio-group {:label       "Column variable:"
-                                                               :this-attr   :table-settings/col-group-variable-uuid
-                                                               :mvi-gv-uuid mvi-gv-uuid}]]))
-                                 (when (>= multi-valued-input-count 2)
-                                   [radio-group {:label         "Row variable:"
-                                                 :attr          :table-settings/row-group-variable-uuid
-                                                 :fallback-attr :graph-settings/z-axis-group-variable-uuid}])
-                                 (when (>= multi-valued-input-count 2)
-                                   [radio-group {:label         "Column variable:"
-                                                 :attr          :table-settings/col-group-variable-uuid
-                                                 :fallback-attr :graph-settings/x-axis-group-variable-uuid}])
-                                 (when (>= multi-valued-input-count 3)
-                                   [radio-group {:label         "Sub-table variable:"
-                                                 :attr          :table-settings/submatrix-group-variable-uuid
-                                                 :fallback-attr :graph-settings/z2-axis-group-variable-uuid}])]}])))
+        group-variables          (map #(deref (subscribe [:wizard/group-variable %])) multi-valued-input-uuids)
+        group-vars               (mapv (fn [{gv-uuid :bp/uuid}]
+                                         {:gv-uuid  gv-uuid
+                                          :var-name @(subscribe [:wizard/gv-uuid->resolve-result-variable-name gv-uuid])})
+                                       group-variables)
+        row-selected             (or (first @(subscribe [:table-settings/attr-values ws-uuid :table-settings/row-group-variable-uuid]))
+                                     (first @(subscribe [:worksheet/get-graph-settings-attr ws-uuid :graph-settings/z-axis-group-variable-uuid])))
+        col-selected             (or (first @(subscribe [:table-settings/attr-values ws-uuid :table-settings/col-group-variable-uuid]))
+                                     (first @(subscribe [:worksheet/get-graph-settings-attr ws-uuid :graph-settings/x-axis-group-variable-uuid])))
+        sub-table-selected       (or (first @(subscribe [:table-settings/attr-values ws-uuid :table-settings/submatrix-group-variable-uuid]))
+                                     (first @(subscribe [:worksheet/get-graph-settings-attr ws-uuid :graph-settings/z2-axis-group-variable-uuid])))
+        row-attr-val             (first @(subscribe [:table-settings/attr-values ws-uuid :table-settings/row-group-variable-uuid]))
+        col-attr-val             (first @(subscribe [:table-settings/attr-values ws-uuid :table-settings/col-group-variable-uuid]))]
+    [c/modal {:title          "Table Settings"
+              :close-on-click #(dispatch [:table-settings/toggle])
+              :content        [:<>
+                               (when (= multi-valued-input-count 1)
+                                 (let [{mvi-uuid :gv-uuid mvi-name :var-name} (first group-vars)]
+                                   [:<>
+                                    [single-mvi-radio-group {:label     "Row variable:"
+                                                             :selected  row-attr-val
+                                                             :mvi-uuid  mvi-uuid
+                                                             :mvi-name  mvi-name
+                                                             :on-select #(dispatch [:table-settings/update-attr ws-uuid :table-settings/row-group-variable-uuid %])}]
+                                    [single-mvi-radio-group {:label     "Column variable:"
+                                                             :selected  col-attr-val
+                                                             :mvi-uuid  mvi-uuid
+                                                             :mvi-name  mvi-name
+                                                             :on-select #(dispatch [:table-settings/update-attr ws-uuid :table-settings/col-group-variable-uuid %])}]]))
+                               (when (>= multi-valued-input-count 2)
+                                 [multi-mvi-radio-group {:label      "Row variable:"
+                                                         :selected   row-selected
+                                                         :on-select  #(dispatch [:table-settings/update-attr ws-uuid :table-settings/row-group-variable-uuid %])
+                                                         :group-vars group-vars}])
+                               (when (>= multi-valued-input-count 2)
+                                 [multi-mvi-radio-group {:label      "Column variable:"
+                                                         :selected   col-selected
+                                                         :on-select  #(dispatch [:table-settings/update-attr ws-uuid :table-settings/col-group-variable-uuid %])
+                                                         :group-vars group-vars}])
+                               (when (>= multi-valued-input-count 3)
+                                 [multi-mvi-radio-group {:label      "Sub-table variable:"
+                                                         :selected   sub-table-selected
+                                                         :on-select  #(dispatch [:table-settings/update-attr ws-uuid :table-settings/submatrix-group-variable-uuid %])
+                                                         :group-vars group-vars}])]}]))
